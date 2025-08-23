dd2a9f84f18038e30596941937b86c80341c6931
roy <loitp@galaxyjoy.vn>
2025-08-23 13:01:51 +0700

    Add Glide and implement background image in MainActivity
    - Added Glide dependency for image loading.
    - Changed `MainActivity` layout from `ConstraintLayout` to `FrameLayout`.
    - Added an `ImageView` for the background and a `Button` to start the game.
    - Loaded a GIF as the background using Glide in `MainActivity`.
    - Set an `OnClickListener` for the play button to start `ChessBoardActivity`.
    - Removed `onClick` attribute from the button in XML and handled it programmatically.
    - Added a new GIF resource `ic_bkg_1.gif`.

--- app/src/main/java/com/saigonphantomlabs/MainActivity.java
+++ app/src/main/java/com/saigonphantomlabs/MainActivity.java
@@ -5,7 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.saigonphantomlabs.chess.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,10 +18,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.a_main);
+        setupViews();
     }
 
-    public void btnPlayClick(View view) {
-        Intent switchActivityIntent = new Intent(this, ChessBoardActivity.class);
-        startActivity(switchActivityIntent);
+    private void setupViews() {
+        Button btnPlay = findViewById(R.id.btnPlay);
+        ImageView ivBkg = findViewById(R.id.ivBkg);
+        Glide.with(this)
+                .asGif()
+                .load(R.drawable.ic_bkg_1) // ảnh gif trong drawable/raw hoặc link URL
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(ivBkg);
+        btnPlay.setOnClickListener(view -> {
+            Intent switchActivityIntent = new Intent(this, ChessBoardActivity.class);
+            startActivity(switchActivityIntent);
+        });
     }
 }
